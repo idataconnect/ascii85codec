8ab0709f140ea1400e5241cbed7001164c7854f8
benupsavs <benupsavs@e73cd0b2-5df1-436a-ae6b-f158e8bd7078>
2010-02-05 02:16:51 +0000

    Minor cleanups.
    git-svn-id: https://svn.java.net/svn/ascii85codec~svn@25 e73cd0b2-5df1-436a-ae6b-f158e8bd7078

--- trunk/Ascii85Codec/test/com/idataconnect/lib/ascii85codec/Ascii85InputStreamTest.java
+++ trunk/Ascii85Codec/test/com/idataconnect/lib/ascii85codec/Ascii85InputStreamTest.java
@@ -28,6 +28,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
+
 package com.idataconnect.lib.ascii85codec;
 
 import java.io.ByteArrayInputStream;
@@ -53,7 +54,7 @@ public class Ascii85InputStreamTest extends TestCase {
         test1 = ("<~9jqo^BlbD-BleB1DJ+*+F(f,q/0JhKF<GL>Cj@.4Gp$d7F!,L7@<6@)/0JDEF<G%<+EV:2F!,\n"
                 + "O<DJ+*.@<*K0@<6L(Df-\\0Ec5e;DffZ(EZee.Bl.9pF\"AGXBPCsi+DGm>@3BB/F*&OCAfu2/AKY\n"
                 + "i(DIb:@FD,*)+C]U=@3BN#EcYf8ATD3s@q?d$AftVqCh[NqF<G:8+EV:.+Cf>-FD5W8ARlolDIa\n"
-                + "l(DId<j@<?3r@:F%a+D58'ATD4$Bl@l3De:,-DJs`8ARoFb/0JMK@qB4^F!,R<AKZ&-DfTqBG%G"
+                + "l(DId<j@<?3r@:F%a+D58'ATD4$Bl@l3De:,-DJs`8ARoFb/0JMK@qB4^F!,R<AKZ&-DfTqBG%G\n"
                 + ">uD.RTpAKYo'+CT/5+Cei#DII?(E,9)oF*2M7/c~>\n").getBytes("US-ASCII");
         test2 = new byte[test1.length + 1];
         System.arraycopy(test1, 0, test2, 0, 7);
